7f216133b887f294076b0dc8373da84c37e04338
melthrd <57990729+melthrd@users.noreply.github.com>
2019-11-20 12:48:28 +0100

    Add files via upload

---
+++ mode de jeu+nom joueurs.c
@@ -0,0 +1,31 @@
+#include "header.h"
+
+int choix_modes (int choixa)
+{
+    printf("Choisir mode de jeu \n \n");
+    printf("1. Jouer en mode normal \n");
+    printf("2. Jouer en mode degrade \n");
+    printf("Que choisissez-vous : ");
+
+    scanf("%d", &choixa);
+    return choixa;
+}
+
+int choix_mode()
+{
+    int choix_mode1;
+    choix_mode1 = choix_modes(0);
+
+    switch (choix_mode1)
+    {
+    case 1 :
+        system("cls");
+        printf("Vous etes en mode normal");
+        break;
+    case 2 :
+        system("cls");
+        printf("Vous etes en mode degrade");
+        break;
+    }
+
+}
