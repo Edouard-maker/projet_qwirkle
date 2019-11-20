5e36f249357a65dc4f1c8334e83220fc411ad574
melthrd <57990729+melthrd@users.noreply.github.com>
2019-11-20 14:29:08 +0100

    Update jouer.c

--- jouer.c
+++ jouer.c
@@ -1,29 +1,62 @@
-/*#include "header.h"
+#include "header.h"
 
-int choix_nb_joueurs (int choixb)
+int choix_nb_joueurs()
 {
-    printf("Entrer nombre de joueurs : \n");
-    printf("Il y a 2, 3 ou 4 joueurs : \n");
-    printf("Entrer votre choix :");
+    int choixb=0;
 
-    scanf ("%d", choixb);
+    printf("Entrer le nombre de joueurs(de 2 a 4 joueurs): \n");
+    scanf("%d",&choixb);
+    while((choixb<2)||(choixb>4))
+    {
+    printf("Entrer le nombre de joueurs(de 2 a 4 joueurs): \n");
+    scanf("%d",&choixb);
+    }
     return choixb;
 }
 
-int choix_nb ()
+int choix_nb()
 {
     int choix_nb1;
-    choix_nb1 = choix_nbs(0);
+    char joueur1[20];
+    char joueur2[20];
+    char joueur3[20];
+    char joueur4[20];
 
-    switch (choix_nb1)
+    switch(choix_nb1)
     {
-
-
-
-
-
-
+        case 2 :
+            printf("Joueur1, quel est votre pseudo? ");
+            scanf("%s",&joueur1);
+            printf("Joueur2, quel est votre pseudo? ");
+            scanf("%s",&joueur2);
+            printf("\n");
+            printf("Bienvenue a vous %s et %s\n", joueur1,joueur2);
+            break ;
+        case 3  :
+            printf("Joueur1, quel est votre pseudo? ");
+            scanf("%s",&joueur1);
+            printf("Joueur2, quel est votre pseudo? ");
+            scanf("%s",&joueur2);
+            printf("Joueur3, quel est votre pseudo? ");
+            scanf("%s",&joueur3);
+            printf("\n");
+            printf("Bienvenue a vous %s, %s et %s\n", joueur1,joueur2,joueur3);
+            break ;
+        case 4   :
+            printf("Joueur1, quel est votre pseudo? ");
+            scanf("%s",&joueur1);
+            printf("Joueur2, quel est votre pseudo? ");
+            scanf("%s",&joueur2);
+            printf("Joueur3, quel est votre pseudo? ");
+            scanf("%s",&joueur3);
+            printf("Joueur4, quel est votre pseudo? ");
+            scanf("%s",&joueur4);
+            printf("\n");
+            printf("Bienvenue a vous %s, %s, %s et %s\n", joueur1,joueur2,joueur3,joueur4);
+            break ;
+        default : printf("Faites votre choix ");
     }
-
+return(choix_nb1);
 }
-*/
+
+
