5aaa2d1259137f26ad07aac30289c80deb5767c4
melthrd <57990729+melthrd@users.noreply.github.com>
2019-11-20 12:47:55 +0100

    Add files via upload

---
+++ jouer.c
@@ -0,0 +1,29 @@
+/*#include "header.h"
+
+int choix_nb_joueurs (int choixb)
+{
+    printf("Entrer nombre de joueurs : \n");
+    printf("Il y a 2, 3 ou 4 joueurs : \n");
+    printf("Entrer votre choix :");
+
+    scanf ("%d", choixb);
+    return choixb;
+}
+
+int choix_nb ()
+{
+    int choix_nb1;
+    choix_nb1 = choix_nbs(0);
+
+    switch (choix_nb1)
+    {
+
+
+
+
+
+
+    }
+
+}
+*/
