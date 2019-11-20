fd1b7d89a746752319da01a347e3959f2ada5010
melthrd <57990729+melthrd@users.noreply.github.com>
2019-11-20 12:42:30 +0100

    Create header.h

---
+++ header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int choixmenu(int choix); // sous programme du choix menu
+void regles();
+int choix_modes (int choixa);
+int choix_mode();
+int choix_nb_joueurs (int choixb);
+int choix_nb();
+
+
+
+
+
+#endif // HEADER_H_INCLUDED
