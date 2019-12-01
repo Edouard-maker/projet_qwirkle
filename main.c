2874afa81900af0431b646bfaa606f6302983cf7
melthrd <57990729+melthrd@users.noreply.github.com>
2019-12-01 18:15:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,43 @@
 #include "header.h"
+
+// fonction pour afficher le menu et faire son choix
+
 int main()
 {
-    int choix1;
+    Color(0,4);
 
-    choix1=choixmenu(0);
+    int choix;
 
+    printf("\n\n\n\n\n\n\t\t\t\t\t\t\tQUIRKLE\n\n\n\n\n\n\n\n\n");
 
-    switch(choix1)
+    printf("\t\t\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",0xC9,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xBB);
+
+    printf("\t\t\t\t\t%c\t\t MENU\t\t\t%c\n",0xBA,0xBA);
+
+    printf("\t\t\t\t\t%c\t\t\t\t\t%c\n",0xBA,0xBA);
+
+    printf("\t\t\t\t\t%c\t%c 1. JOUER UNE PARTIE \t\t%c\n",0xBA,0x10,0xBA);
+
+    printf("\t\t\t\t\t%c\t%c 2. REGLES DU JEU \t\t%c\n",0xBA,0x10,0xBA);
+
+    printf("\t\t\t\t\t%c\t%c 3. SCORES \t\t\t%c\n",0xBA,0x10,0xBA);
+
+    printf("\t\t\t\t\t%c\t%c 4. QUITTER \t\t\t%c\n",0xBA,0x10,0xBA);
+
+    printf("\t\t\t\t\t%c\t\t\t\t\t%c\n",0xBA,0xBA);
+
+    printf("\t\t\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",0xC8,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xCD,0xBC);
+
+    printf("\n\t\t\t\t\tQue voulez vous faire ? ");
+    scanf("%d",&choix);
+
+    while((choix<1)||(choix>4))
+        {
+        printf("\nVeuillez entrer un nombre entre 1 et 4 a l'ecran s'il vous plait.\nQue voulez vous faire?\n");
+        scanf("%d",&choix);
+        }
+
+    switch(choix)
     {
         case 1 :
             system("cls");
@@ -23,9 +54,10 @@ int main()
             break;
         case 4 :
             system("cls");
-            printf("%c QUITTER %c",22,22);
+            printf("%c A UNE PROCHAINE FOIS %c",0x03,0x03);
             break;
-        default : printf("Faites votre choix ");
+        default :
+            printf("Faites votre choix ");
     }
     return 0;
 }
